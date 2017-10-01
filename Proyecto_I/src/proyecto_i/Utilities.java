d86ee80f049bf5764065939929476e96a98df3bc
Harry Caballeros <hscaballeros@correo.url.edu.gt>
2017-10-01 12:02:59 -0600

    A lot of things.
    Back-Up Finalized.
    Modify Profile (Remove Line)
    Spanglish kind of Arranged.
    Minor Fixes or Bugs.
    LayOut of Forms Modified.

--- Proyecto_I/src/proyecto_i/Utilities.java
+++ Proyecto_I/src/proyecto_i/Utilities.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
