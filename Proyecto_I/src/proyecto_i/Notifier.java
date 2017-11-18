0b990fb7949507f47adba195cdcfd6b1b852ccec
Sebastián Orantes Chang <ater7991@gmail.com>
2017-11-18 12:02:22 -0600

    connection Ok :)

--- Proyecto_I/src/proyecto_i/Notifier.java
+++ Proyecto_I/src/proyecto_i/Notifier.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dbprueba;
+package proyecto_i;
 
 import java.sql.Connection;
 import java.sql.SQLException;
