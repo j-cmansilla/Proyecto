f2cc7c0c9aad8c04bdefd5c97b64036000ab4bae
Sebastián Orantes Chang <ater7991@gmail.com>
2017-11-18 12:21:44 -0600

    all good :+1:

--- Proyecto_I/src/proyecto_i/Dbprueba.java
+++ Proyecto_I/src/proyecto_i/Dbprueba.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dbprueba;
+package proyecto_i;
 
 import java.io.PrintWriter;
 import java.sql.*;
