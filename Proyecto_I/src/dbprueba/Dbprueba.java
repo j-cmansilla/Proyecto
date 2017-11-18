3fe2a4937d6773d970e64f6624aa34ba2b3dc8e0
Sebastián Orantes Chang <ater7991@gmail.com>
2017-11-18 12:10:38 -0600

    Revert "connection Ok :)"
    This reverts commit 0b990fb7949507f47adba195cdcfd6b1b852ccec.

--- Proyecto_I/src/dbprueba/Dbprueba.java
+++ Proyecto_I/src/dbprueba/Dbprueba.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto_i;
+package dbprueba;
 
 import java.io.PrintWriter;
 import java.sql.*;
